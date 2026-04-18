72d40335483dd406bef25e87df40d1ae85116775
durgasahusahu <durgasahusahu1@gmail.com>
2026-04-18 22:38:18 +0530

    created customEror sucessResponse corsConfig files

--- src/app.js
+++ src/app.js
@@ -1,5 +1,33 @@
 import express from  "express";
 import morgan from "morgan";
-import dotenv from "dotenv"
-import notFound from "./common/middleware/notFound";
-import errorHandler from "./common/middleware/errorHandler";
+import notFound from "./common/middleware/notFound.js";
+import errorHandler from "./common/middleware/errorHandler.js";
+import successResponse from "./common/utils/sucessResponse.js";
+import cookieParser from "cookie-parser"
+import corsConfig from "./config/corsConfig.js";
+import routes from "./routes.js"
+
+
+const app = express();
+
+
+app.use(morgan("dev"));
+app.use(corsConfig)
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use("/api/v1",routes);
+
+app.get("/",(req,res) => {
+    successResponse(res,{},"API IS WOKING")
+})
+
+
+
+app.use(notFound);
+app.use(errorHandler)
+
+
+export default app;
+
---
+++ src/config/corsConfig.js
@@ -0,0 +1,32 @@
+import cors from "cors";
+
+const isDev = () => process.env.NODE_ENV === "development";
+
+const getAllowedOrigins = () => {
+  if (isDev()) {
+    return ["http://localhost:5173","http://localhost:4173" , "http://localhost:5174"];
+  } else {
+    return [
+      "https://lenstick-frontend-admin.vercel.app",
+      "https://lens-stick.vercel.app"
+    ];
+  }
+};
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    const allowedOrigins = getAllowedOrigins();
+
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  credentials: true,
+};
+
+export default cors(corsOptions);
